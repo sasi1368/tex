6cf170a4cad2329c592056881b210d18a9fd508b
sasi1368 <sasansasanione@gmail.com>
2025-04-04 18:52:39 +0330

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const User = require("../models/user");
+const User = require("../models/User");
 
 // ارسال کد
 router.post("/send-code", async (req, res) => {
