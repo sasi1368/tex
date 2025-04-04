8e1aa82c289e042230f221be89f15c19d39a345e
sasi1368 <sasansasanione@gmail.com>
2025-04-04 20:05:57 +0330

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,10 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
+const User = require('../models/User');
 
-router.get("/users", async (req, res) => {
-  const users = await User.find().sort({ createdAt: -1 });
-  res.json(users);
+// گرفتن اطلاعات کاربران
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ success: true, users });
+  } catch (error) {
+    console.error('❌ خطا در دریافت اطلاعات کاربران:', error);
+    return res.status(500).json({ success: false, error: 'Server error' });
+  }
 });
 
 module.exports = router;
